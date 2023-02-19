1eb0ee3bddfcfb86a69e83a2653c7ca9ffbedc2f
Vladimir Komarov <vvkcrtc@gmail.com>
2023-02-19 18:36:58 +0300

    Change JavaConfig

--- src/main/java/ru/netology/springboot1/config/JavaConfig.java
+++ src/main/java/ru/netology/springboot1/config/JavaConfig.java
@@ -9,13 +9,14 @@ import ru.netology.springboot1.SystemProfile.SystemProfile;
 @Configuration
 public class JavaConfig {
     @Bean
-    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true")
+    //@ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true")
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    @ConditionalOnProperty(prefix = "netology.profile", name = "dev",  havingValue = "false")
+    @ConditionalOnProperty(value = "netology.profile.dev",  havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
